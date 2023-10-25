044914691c0859d48951bd7b62376353c7e57b05
lineMarques <aline.marques18@gmail.com>
2023-10-25 19:44:36 -0300

    refatorado/ nome de variaveis semanticas + botão refresh

--- index.js
+++ index.js
@@ -8,41 +8,41 @@ function cifraDeCesar() {
 
     const TxEscolha = document.querySelector("input[name=nmTxEscolha]:checked").value;
 
-    /* Dados Validados */
-    let vl = validaDados();
-    if (vl.flag == true) {
+    /* validados são os dados validados */
+    let validados = validaDados();
+    if (validados.flag == true) {
       return;
     }
 
     if (TxEscolha == "cifrado") {
-
-      for (let index = 0; index < vl.texto.value.length; index++) {
+      for (let index = 0; index < validados.texto.value.length; index++) {
         /* Trabalhando com numeros da tabela ASCII.
-          codigo = (Valor da letra - valor da primeira letra do vl.alfabeto) + valor da vl.cifra
+          codigo = (Valor da letra - valor da primeira letra do validados.alfabeto) + valor da validados.cifra
           modulo é usado para quando o valor passar de 26
-          usar o resto para voltar ao inicio do vl.alfabeto
+          usar o resto para voltar ao inicio do validados.alfabeto
          */
 
         let codigo =
-          (vl.texto.value[index].toLowerCase().charCodeAt(0) - vl.alfabeto[0].charCodeAt(0) + Number(vl.cifra)) % 26;
-        encoding += vl.alfabeto[codigo];
+          (validados.texto.value[index].toLowerCase().charCodeAt(0) - validados.alfabeto[0].charCodeAt(0) + Number(validados.cifra)) %26;
+
+        encoding += validados.alfabeto[codigo];
       }
-      vl.texto.value = encoding;
+
+      validados.texto.value = encoding;
       encoding = "";
-
     } else {
+      for (let index = 0; index < validados.texto.value.length; index++) {
+        
+        let reverteCodigo = 
+        (validados.texto.value[index].charCodeAt(0) -  validados.alfabeto[0].charCodeAt(0) + 26 - Number(validados.cifra)) %26;
 
-      for (let index = 0; index < vl.texto.value.length; index++) {
-        let reverteCodigo =
-          (vl.texto.value[index].charCodeAt(0) - vl.alfabeto[0].charCodeAt(0) + 26 - Number(vl.cifra)) % 26;
-        descoding += vl.alfabeto[reverteCodigo];
+        descoding += validados.alfabeto[reverteCodigo];
       }
-      vl.texto.value = descoding;
+      validados.texto.value = descoding;
       descoding = "";
-      
     }
   });
-}
+}cifraDeCesar();
 
 function validaDados() {
   /* Entradas de dados */
@@ -54,7 +54,7 @@ function validaDados() {
   let output = document.getElementById("idSaida");
   output.innerText = "";
 
-  /* Validando texto: se regex der false da msg de erro e retorna */
+  /* Validando texto: se regex der false aparece msg de erro e retorna */
   for (let index = 0; index < texto.value.length; index++) {
     let regex = new RegExp("[a-zA-Z]").test(texto.value[index]);
 
@@ -64,9 +64,9 @@ function validaDados() {
     }
   }
 
-  /*Validando vl.Cifra: laço para percorrer vl.alfabeto validando o valor digitado na variavel vl.cifra */
+  /*Validando validados.Cifra: laço para percorrer validados.alfabeto validando o valor digitado na variavel validados.cifra */
   for (let index = 0; index < alfabeto.length; index++) {
-    /* Validando que a variavel vl.cifra está entre as letras a e i e entre os numeros 1 e 9. */
+    /* Validando que a variavel validados.cifra está entre as letras a e i e entre os numeros 1 e 9. */
     if (cifra === alfabeto.slice(9)[index] || cifra < 1 || cifra > 9) {
       output.innerText = "Valor fora do Range";
       flag = true;
@@ -80,4 +80,11 @@ function validaDados() {
 
   return { texto, cifra, alfabeto, flag };
 }
-cifraDeCesar();
+
+function refreshPagina(){
+  const btnRefresh = document.getElementById('idBtnRfresh')
+  btnRefresh.addEventListener('click', function(){
+    location.reload();
+  });
+} refreshPagina()
+
